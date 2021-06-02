c634df326c693f7870cbf57b37b8bba84b3ba996
Omrron <obaron4120@gmail.com>
2021-06-02 15:19:03 +0300

    added guest constructors

--- GoldGuests.java
+++ GoldGuests.java
@@ -1,6 +1,10 @@
 public class GoldGuests extends Guests{
     private boolean bonus;
 
+    public GoldGuests(String name, int totalnumber, int numKids) {
+        super(name, totalnumber, numKids);
+    }
+
     public double getMult() {
         return 0.75;
     }
