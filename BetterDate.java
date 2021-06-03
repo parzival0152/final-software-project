807da117831389f0dabde552d2e1cab9cb3847c5
Or Reginiano <81361496+or2407@users.noreply.github.com>
2021-06-03 19:04:59 +0300

    Update BetterDate.java

---
+++ BetterDate.java
@@ -0,0 +1,49 @@
+import java.util.Date;
+
+public class BetterDate extends Date{
+
+    public BetterDate()
+    {
+        super();
+        Date temp = new Date(2021, 05, 0, 0, 0);
+        this.setDate(temp.getDate());
+    }
+    
+    public BetterDate(int month, int day)
+    {
+        super();
+        Date temp = new Date(2021, month, day);
+        this.setDate(temp.getDate());
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.valueOf(getDate());
+    }
+
+    @Override
+    public int getDay()
+    {
+        return super.getDate();
+    }
+
+    public void setDay(int day)
+    {
+        super.setDate(day);
+    }
+
+    @Override
+    public int getMonth()
+    {
+        return super.getMonth();
+    }
+
+    @Override
+    public void setMonth(int month)
+    {
+        super.setMonth(month);
+    }
+
+
+}
