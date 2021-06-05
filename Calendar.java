dc4072b33c25f4cc16256e693826cd7540eeb094
Or Reginiano <81361496+or2407@users.noreply.github.com>
2021-06-05 22:57:01 +0300

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -5,76 +5,104 @@ import java.util.Map;
 public class Calendar {
 
     Object[] dateArr;
-    Map<BetterDate,ArrayList<Booking>> bookMap;
-    
+    Map<String ,Booking> bookMap;
+    ArrayList<Integer> roomsType[];
 
     public Calendar()
     {
+        for(int i=0; i<20; i++)
+        {
+            if (i>4&&(i%4==0||i%4==3)&&i<17)
+                roomsType[1].add(i);
+            else if (i>=17)
+                roomsType[2].add(i);
+            else 
+                roomsType[0].add(i);
+        }
 
     }
-    public void addBooking(BetterDate start, BetterDate finish, String name, int num_guest, int room_type)
+    public void addBooking(String name, BetterDate start, BetterDate finish, int num_guest, int room_type, int room_num, int guests_num)
     {
+        ArrayList<Integer> roomArr= new ArrayList<Integer>();
+        ArrayList<Integer> selectRoom= new ArrayList<Integer>();
+        int counter=0;
+
+        //this is the list of the rooms that are not available
+        roomArr= findRooms(start, finish);
+        
+
+        for(int i=0; i<roomsType[room_type].size();i++)
+        {
+            //if one of the rooms are not in roomArr, add to selectRoom
+            if(!roomArr.contains(roomsType[room_type].get(i)))
+            {
+                selectRoom.add(roomsType[room_type].get(i));
+                counter++;
+                if(counter==room_num)
+                    break;
+            }
+        }
+
+        if(counter==room_num)
+        {
+            //create booking
+            Booking new1 = new Booking(name, selectRoom, guests_num, start, finish);
+            bookMap.put(name, new1);
+        }
+
 
     }
 
-    //public Booking findBooking(String booking_Guest)
-     //{
-
-
-
-
-
-     //}
-
-
-
-
-    public ArrayList<Integer> findRooms(BetterDate start, BetterDate finish, int room_type)
+    public ArrayList<Integer> findRooms(BetterDate start, BetterDate finish)
     {
         ArrayList<Integer> roomArr= new ArrayList<Integer>();
 
-        for(Map.Entry<BetterDate,ArrayList<Booking>> entry : bookMap.entrySet())
+        for(Map.Entry<String,Booking> entry : bookMap.entrySet())
         {
-            BetterDate start2=entry.getKey();
-
             //go over all bookings in specific starting date
-            for(int index=0; index<entry.getValue().size(); index++)
+
+            BetterDate start2=entry.getValue().getArrival_date();
+            BetterDate finish2=entry.getValue().getLeaving_date();
+
+            //if our desired date starts before another finishes
+            if(start.compareTo(start2)>0&&start.compareTo(finish2)<0)
             {
-                BetterDate finish2=entry.getValue().get(index).getLeaving_date();
+                //go over all the rooms in that booking
+                for(int j=0; j<entry.getValue().getRooms().size(); j++)
+                    roomArr.add(entry.getValue().getRooms().get(j));
+                    
+            }
 
-                //if our desired date starts before another finishes
-                if(start.compareTo(start2)>0&&start.compareTo(finish2)<0)
-                {
-                    //go over all the rooms in that booking
-                    for(int j=0; j<entry.getValue().get(index).getRooms().size(); j++)
-                        roomArr.add(entry.getValue().get(index).getRooms().get(j));
-                        
-                }
-
-                //if our desired date finishes after another starts
-                if(start.compareTo(start2)<0 &&(finish.compareTo(start2)>0))
-                {
-                    for(int j=0; j<entry.getValue().get(index).getRooms().size(); j++)
-                        roomArr.add(entry.getValue().get(index).getRooms().get(j));
-                }
-
+            //if our desired date finishes after another starts
+            if(start.compareTo(start2)<0 &&(finish.compareTo(start2)>0))
+            {
+                for(int j=0; j<entry.getValue().getRooms().size(); j++)
+                    roomArr.add(entry.getValue().getRooms().get(j));
             }
         }
 
         return roomArr;
     }
-    public boolean checkColl(BetterDate start1, BetterDate finish1, BetterDate start2, BetterDate finish2)
+
+    public void deleteBooking(String name)
     {
-        if(start1.compareTo(start2)<0&&finish1.compareTo(start2)<=0)
-            return false;
-        if(start1.compareTo(finish2)>=0&&finish1.compareTo(finish2)>0)
-            return false;
-        return true;
+        bookMap.remove(name);
     }
 
-    public int getStay(int roomId, Guests guest) //return the number of days the guest has booked the room
+    public void editBooking(String name)
     {
-        return 5;
+
+    }
+
+    public void changeDate(String name, BetterDate start, BetterDate finish)
+    {
+        bookMap.get(name).setArrival_date(start);
+        bookMap.get(name).setLeaving_date(finish);
+    }
+
+    public void chengeGuestNum(String name, int guests_num)
+    {
+        bookMap.get(name).setNum_Guests(guests_num);
     }
     
 
