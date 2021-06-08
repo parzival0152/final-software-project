90cb98d852a398bb9e13f0197ffefabdfd376564
Omrron <obaron4120@gmail.com>
2021-06-08 13:39:26 +0300

    removed guest types

--- Guests.java
+++ Guests.java
@@ -2,11 +2,13 @@ public abstract class Guests {
     private String name;
     private int totalnumber;
     private int numKids;
+    private int type;
 
-    public Guests(String name, int totalnumber, int numKids) {
+    public Guests(String name, int totalnumber, int numKids, int type) {
         this.name = name;
         this.totalnumber = totalnumber;
         this.numKids = numKids;
+        this.type = type;
     }
 
     public abstract double getMult();
@@ -34,5 +36,18 @@ public abstract class Guests {
 
     public void setNumKids(int numKids) {
         this.numKids = numKids;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public double discount(){
+        if(this.getType() == 0)
+            return 1.0;
+        else if (this.getType() == 1)
+            return 0.85;
+        else
+            return 0.75;
     }
 }
