9cd92b4a2aa3024df8a7097f1ba9081ed2785c69
Omrron <obaron4120@gmail.com>
2021-06-14 17:38:21 +0300

    UI changes
    now with buttons

--- UI.java
+++ UI.java
@@ -22,28 +22,16 @@ public class UI {
         //             "Error: not an option\nPlease choose one of the following options:\n" + message + "\n"));
         // }
         // // do while there are choices
-        return JOptionPane.showOptionDialog(null, "Returns the position of your choice on the array",
-        "Click a button",
+        return JOptionPane.showOptionDialog(null, "Choose an option",
+    "MA Continental the hotel for you",
         JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0])+1;
     }
 
-    public static int askOption(ArrayList<String> options){
-        int choice = -1;
-        String message = "";
-        // options are given in selected class and are sent here
-        for (int i = 0; i < options.size(); i++) {
-            message = message + Integer.toString(i + 1) + ") " + options.get(i) + "\n";
-        }
-        choice = Integer.parseInt(
-                JOptionPane.showInputDialog("Please choose one of the following options:\n" + message + "\n"));
-
-        // sending back to class selection by user
-        while (!(choice > 0 && choice <= options.size())) {
-            choice = Integer.parseInt(JOptionPane.showInputDialog(
-                    "Error: not an option\nPlease choose one of the following options:\n" + message + "\n"));
-        }
-        // do while there are choices
-        return choice;
+    public static int askOption(ArrayList<String> loptions){
+        Object[] options = loptions.toArray();
+        return JOptionPane.showOptionDialog(null, "Choose an option",
+    "MA Continental the hotel for you",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0])+1;
     }
 
     public static String askString(String prompt) {
